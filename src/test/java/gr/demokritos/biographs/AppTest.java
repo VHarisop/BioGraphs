821146d4fd298199671dead4886136619bf4e6db
VHarisop <the.machine.headed@gmail.com>
2015-12-25 02:56:31 +0200

    Added test file

--- src/test/java/gr/demokritos/biographs/AppTest.java
+++ src/test/java/gr/demokritos/biographs/AppTest.java
@@ -10,9 +10,13 @@ import gr.demokritos.iit.jinsect.structs.Edge;
 import gr.demokritos.iit.jinsect.structs.JVertex;
 import gr.demokritos.iit.jinsect.structs.NGramVertex;
 import gr.demokritos.iit.jinsect.utils;
+import gr.demokritos.iit.jinsect.jutils;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.LinkedHashMap;
 
 /**
@@ -75,12 +79,11 @@ public class AppTest
 	 */
 	public void testFasta() 
 	{
-		BioJGraph bgx = null;
 		String fName = "/testFile01.fasta";
 		assertNotNull("Test file missing", getClass().getResource(fName));
 
 		// labels of sequences in fasta file
-		String[] labels = new String[] {"AB000263", "AB00264", "AB00265"};
+		String[] labels = new String[] {"AB00265"};
 
 		try {
 			File res = new File(getClass().getResource(fName).toURI());
@@ -92,6 +95,29 @@ public class AppTest
 				assertNotNull(b.bioLabel);
 				assertTrue(b.bioLabel.equals(labels[labelCnt++]));
 			}
+		} 
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		fName = "/1099_consistent_NFR.fa";
+		assertNotNull("Test file missing", getClass().getResource(fName));
+
+		try {
+			File res = new File(getClass().getResource(fName).toURI());
+			BioJGraph[] bgs = BioJGraph.fastaFileToGraphs(res);
+
+			// assert that all graphs have been read
+			assertTrue(bgs.length == 1099);
+			for (BioJGraph b: bgs) {
+				assertNotNull(b);
+				assertNotNull(b.bioLabel);
+			}
+
+			// assert that labels have been read properly
+			assertTrue(bgs[0].bioLabel.equals("chr1:37519-37552"));
+			assertTrue(bgs[2].bioLabel.equals("chr1:38881-38901"));
+			assertTrue(bgs[5].bioLabel.equals("chr1:52499-52513"));
 		} 
 		catch (Exception ex) {
 			ex.printStackTrace();
@@ -109,7 +135,6 @@ public class AppTest
 		SimilarityDatabase gSimData = new SimilarityDatabase();
 		try {
 			File res = new File(getClass().getResource(fName).toURI());
-			System.out.println(res.isDirectory());
 			gData.buildIndex(res);
 			gSimData.buildIndex(res);
 		}
@@ -120,6 +145,52 @@ public class AppTest
 		// make sure 3 different keys exist
 		assertTrue(gData.exposeKeys().size() == 3);
 		assertTrue(gSimData.exposeKeys().size() == 3);
+	}
+
+	/**
+	 * Test that index building and retrieval works properly
+	 * for {@link SimilarityDatabase} classes.
+	 */
+	public void testRetrieval() {
+		String sTest = "/testFile01.fasta";
+		String nfrIndex = "/1099_consistent_NFR.fa";
+		String nclIndex = "/3061_consistent_nucleosomes.fa";
+
+		// String sIndex = "/testFile02.fasta";
+
+		SimilarityDatabase nfrData = new SimilarityDatabase();
+		SimilarityDatabase nclData = new SimilarityDatabase();
+
+		try {
+			// build database index 
+			File resNFR = new File(getClass().getResource(nfrIndex).toURI());
+			File resNCL = new File(getClass().getResource(nclIndex).toURI());
+			
+			nfrData.buildIndex(resNFR);
+			nclData.buildIndex(resNCL);
+
+			// build the test graph
+			File res = new File(getClass().getResource(sTest).toURI());
+			BioJGraph bgTest = BioJGraph.fromFastaFile(res);
+
+			// assert that querying an existing graph gives non-null labels
+			List<String> labels = nfrData.treeIndex.get(bgTest);
+			assertNotNull(labels); 
+			
+			// assert that the existing graph is found in the returned list
+			boolean found = false;
+			for (String s: labels) {
+				if (s.equals("chr1:39666-39676")) {
+					found = true;
+					break;
+				}
+			}
+			assertTrue(found);
+
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	/**
