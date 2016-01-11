44a3d3580e8d88e27154b31cbad6825a6f901e28
VHarisop <the.machine.headed@gmail.com>
2016-01-12 01:43:53 +0200

    Added copyright/license on top of every file

--- src/main/java/gr/demokritos/biographs/SimilarityComparator.java
+++ src/main/java/gr/demokritos/biographs/SimilarityComparator.java
@@ -1,3 +1,19 @@
+/* Copyright (C) 2016 VHarisop
+ * This file is part of BioGraphs.
+ *
+ * BioGraphs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BioGraphs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BioGraphs.  If not, see <http://www.gnu.org/licenses/>. */
+
 package gr.demokritos.biographs;
 
 import java.util.Comparator;
