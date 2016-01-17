e1aaa14538075d76a2e03a2d3497977856f74ecf
VHarisop <the.machine.headed@gmail.com>
2016-01-17 19:52:33 +0200

    Updated main method to check comparators

--- src/main/java/gr/demokritos/biographs/Main.java
+++ src/main/java/gr/demokritos/biographs/Main.java
@@ -1,6 +1,8 @@
 package gr.demokritos.biographs;
 
 import gr.demokritos.biographs.indexing.TreeDatabase;
+import gr.demokritos.biographs.indexing.CanonicalCodeComparator;
+import gr.demokritos.biographs.indexing.TwoLevelSimComparator;
 import gr.demokritos.iit.jinsect.jutils;
 
 import java.io.File;
@@ -15,13 +17,24 @@ public class Main {
 			return; 
 		}
 
-		/* Create a simple similarity database */
-		TreeDatabase<BioGraph> trd = new TreeDatabase<BioGraph>() {
-			@Override
-			public BioGraph getGraphFeature(BioGraph bG) {
-				return bG;
-			}
-		};		
+		/* Create a TreeDatabase ordered by canonical coding */
+		TreeDatabase<BioGraph> trdCanon = 
+			new TreeDatabase<BioGraph>(new CanonicalCodeComparator()) {
+				@Override
+				public BioGraph getGraphFeature(BioGraph bG) {
+					return bG;
+				}
+			};	
+
+		/* Create a TreeDatabase ordered by a two-level comparator */
+		TreeDatabase<BioGraph> trdSim = 
+			new TreeDatabase<BioGraph>(new TwoLevelSimComparator()) {
+				@Override
+				public BioGraph getGraphFeature(BioGraph bG) {
+					return bG;
+				}
+			};
+		/* set the database to use a two level comparator */
 
 		BioGraph[] bgs;
 		/* try to get the files and build the index and the biographs
@@ -30,7 +43,8 @@ public class Main {
 			File dataFile = new File(args[0]);
 			File testFile = new File(args[1]);
 
-			trd.buildWordIndex(dataFile);
+			trdCanon.buildWordIndex(dataFile);
+			trdSim.buildWordIndex(dataFile);
 			bgs = BioGraph.fromWordFile(testFile);
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -38,8 +52,21 @@ public class Main {
 		}
 		
 		double sSim;
+		System.out.println("CCODE:\n");
 		for (BioGraph bg: bgs) {
-			List<BioGraph> near = trd.getNearestNeighbours(bg, true);
+			List<BioGraph> near = trdCanon.getKNearestNeighbours(bg, true, 2);
+			System.out.printf("%s: \n", bg.getLabel());
+			for (BioGraph b: near) {
+				sSim = 
+					jutils.graphStructuralSimilarity(bg.getGraph(), b.getGraph());
+				System.out.printf("\t%s - %.3f\n", b.getLabel(), sSim);
+			}
+		}
+		System.out.println("\n\n");
+
+		System.out.println("2SIM:\n");
+		for (BioGraph bg: bgs) {
+			List<BioGraph> near = trdSim.getKNearestNeighbours(bg, true, 2);
 			System.out.printf("%s: \n", bg.getLabel());
 			for (BioGraph b: near) {
 				sSim = 
