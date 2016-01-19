71c0f0f82790089feb6d162334949a4202943858
VHarisop <the.machine.headed@gmail.com>
2016-01-19 23:41:19 +0200

    Updated KnnTest to test recall

--- src/test/java/gr/demokritos/biographs/indexing/KnnTest.java
+++ src/test/java/gr/demokritos/biographs/indexing/KnnTest.java
@@ -1,6 +1,7 @@
 package gr.demokritos.biographs.indexing;
 
 import gr.demokritos.biographs.*;
+import gr.demokritos.iit.jinsect.jutils;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -65,6 +66,55 @@ public class KnnTest
 			bgTest = BioGraph.fromFastaFile(test);
 
 			assertNotNull(bgTest);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			assertTrue(false);
+		}
+	}
+
+	public void testRecall() {
+		String wordFile = "/words.txt";
+		try {
+			Comparator<BioGraph> bgComp = new Comparator<BioGraph>() {
+				@Override
+				public int compare(BioGraph bgA, BioGraph bgB) {
+					double sSim = 
+						bgA.getGraph().getDegreeRatioSum() -
+						bgB.getGraph().getDegreeRatioSum();
+
+					if (GraphDatabase.compareDouble(sSim, 0.0)) {
+						return jutils.compareCanonicalCodes(
+								bgA.getGraph(),
+								bgB.getGraph());
+					} else {
+						return Double.compare(sSim, 0.0);
+					}
+				}
+			};
+			File res = new File(getClass().getResource(wordFile).toURI());
+			TreeDatabase<String> trd = new TreeDatabase<String>(bgComp) {
+				@Override
+				public String getGraphFeature(BioGraph bG) {
+					return bG.getLabel();
+				}
+			};
+
+			trd.buildWordIndex(res);
+			BioGraph[] bgs = BioGraph.fromWordFile(res);
+
+			/* make sure that graphs that were indexed in the database
+			 * return nonempty K nearest neighbours when queried */
+			for (BioGraph b: bgs) {
+				List<String> matches = 
+					trd.getKNearestNeighbours(b, true, 3);
+				assertNotNull(matches);
+				assertTrue(matches.size() > 0);
+
+				List<String> matches_noninclusive = 
+					trd.getKNearestNeighbours(b, false, 3);
+				assertNotNull(matches_noninclusive);
+				assertTrue(matches_noninclusive.size() > 0);
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			assertTrue(false);
