d341a482bc83e1efa555eddbc251693dd294cc2c
VHarisop <the.machine.headed@gmail.com>
2016-02-15 10:14:44 +0200

    Added bin size printing to script

--- scripts/check_cluster_combos.sh
+++ scripts/check_cluster_combos.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 BASE_DIR="${HOME}/demokritos/BioGraphs"
-MAIN="gr.demokritos.biographs.experiments.TestCluster"
+MAIN="gr.demokritos.biographs.experiments.TestClusterHybrid"
 WORDS="${BASE_DIR}/scripts/datasets/words.txt"
 MUTATIONS="${BASE_DIR}/scripts/word_mutations.txt"
 GROUND_FILE="${BASE_DIR}/scripts/datasets/words.txt_c_1_mutated.txt"
 SCRIPT="${BASE_DIR}/scripts/parse_results.py"
+
+# run a clean compilation
+mvn clean compile
+[[ $? -ne  0 ]] && exit
 
 # Run the experiment for all combos, parse results and remove temp files
 for clusters in 35 30 25 20 10; do
@@ -18,7 +22,11 @@ for clusters in 35 30 25 20 10; do
 		# run python script to parse results
 		python3 ${SCRIPT} -g ${GROUND_FILE} -r test_${clusters}_${iters}.out \
 			| awk -F ' ' '{print $2}' \
-			| xargs printf "Cluster (%d, %d): %s\n" ${clusters} ${iters}
+			| xargs printf "Cluster (%d, %d): %s - " ${clusters} ${iters}
+
+		# print bin sizes as well
+		python3 ${SCRIPT} -g ${GROUND_FILE} -r test_${clusters}_${iters}.out \
+			| awk -F ' ' '{$1=$2=""; print $0}'
 
 		# print bin size and remove temp file
 		rm test_${clusters}_${iters}.out
