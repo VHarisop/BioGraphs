b07614f757da0dfed6fc88eb2f806318269d39ea
VHarisop <the.machine.headed@gmail.com>
2016-02-22 00:54:20 +0200

    Added hashed version of inverted index

---
+++ src/main/java/gr/demokritos/biographs/experiments/TestInvertedHash.java
@@ -0,0 +1,114 @@
+/* Copyright (C) 2016 VHarisop
+ * This file is part of BioGraphs.
+ *
+ * BioGraphs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BioGraphs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BioGraphs.  If not, see <http://www.gnu.org/licenses/>. */
+
+package gr.demokritos.biographs.experiments;
+
+import gr.demokritos.biographs.*;
+import gr.demokritos.biographs.indexing.inverted.*;
+import gr.demokritos.biographs.indexing.structs.Stats;
+import gr.demokritos.biographs.indexing.GraphDatabase.GraphType;
+
+import java.io.File;
+import java.util.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class TestInvertedHash {
+	/**
+	 * Gson builder
+	 */
+	static Gson gson;
+
+	/**
+	 * Files from which to pull database and testing graphs.
+	 */
+	static File testFile = null, dataFile = null;
+	static int tolerance = 3;
+
+	static Stats checkIndex(BioGraph[] bgs, HashedInvertedIndex invInd) {
+		Stats stat = new Stats("hashed_inv_index");
+		long maxTime = 0L, sumTime = 0L;
+		for (BioGraph bg : bgs) {
+			/* measure per-item query time to extract maximum and mean
+			 * query times */
+			long startTime = System.currentTimeMillis();
+			Set<BioGraph> ans = new HashSet<BioGraph>(); 
+			ans.add(invInd.getNearestNeighbour(bg, tolerance));
+			long stopTime = System.currentTimeMillis();
+			if ((stopTime - startTime) > maxTime) {
+				maxTime = stopTime - startTime;
+			}
+			sumTime += stopTime - startTime;
+
+			if (ans == null || ans.size() == 0) {
+				stat.addResult(bg.getLabel(), "None");
+			}
+			else {
+				String[] labels = new String[ans.size()]; int ind = 0;
+				for (BioGraph bIn: ans) {
+					labels[ind++] = bIn.getLabel();
+				}
+				stat.addResult(bg.getLabel(), labels);
+			}
+		}
+		stat.setTimes(maxTime, sumTime, bgs.length);
+		stat.setBins(invInd.binSizes());
+		return stat;
+	}
+
+	public static void main(String[] args) 
+	throws NumberFormatException 
+	{
+		/* if no file was provided as argument, inform the user and exit */
+		if (args.length < 2) {
+			System.out.println("Missing file argument!");
+			return; 
+		}
+		
+		if (args.length >= 3) {
+			tolerance = Integer.parseInt(args[2]);
+		}
+
+		HashedInvertedIndex invInd = new HashedInvertedIndex();
+		BioGraph[] bGraphsTest = null;
+		gson = new GsonBuilder().setPrettyPrinting().create();
+		Stats[] statList = new Stats[1];
+		try {
+			dataFile = new File(args[0]);
+			testFile = new File(args[1]);
+			bGraphsTest = BioGraph.fastaFileToGraphs(testFile);
+
+			/* build index and measure building time */
+			long startTime = System.currentTimeMillis();
+			invInd.build(dataFile, GraphType.DNA);
+			long stopTime = System.currentTimeMillis();
+			System.err.printf("Building: %s s\n",
+					String.valueOf((stopTime - startTime) / 1000.0));
+
+			/* perform query, measure total and per-item query time */
+			startTime = System.currentTimeMillis();
+			statList[0] = checkIndex(bGraphsTest, invInd);
+			stopTime = System.currentTimeMillis();
+			System.err.printf("Total query: %s s\n",
+					String.valueOf((stopTime - startTime) / 1000.0));
+			System.out.println(gson.toJson(statList));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return;
+	}
+}
